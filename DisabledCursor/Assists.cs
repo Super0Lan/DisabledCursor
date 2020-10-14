c4a85e2e9667f789d7b912ce9e198f2e58e7ee9a
390811879@qq.com <WangLan0913>
2020-10-14 11:19:35 +0800

    Init

---
+++ DisabledCursor/Assists.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+namespace DisabledCursor
+{
+    public class Assists
+    {
+        #region 是否使用禁用下的样式设计
+        public static bool GetUseDisabledDesign(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(UseDisabledDesignProperty);
+        }
+
+        public static void SetUseDisabledDesign(DependencyObject obj, bool value)
+        {
+            obj.SetValue(UseDisabledDesignProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for UseDisabledDesign.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UseDisabledDesignProperty =
+            DependencyProperty.RegisterAttached("UseDisabledDesign", typeof(bool), typeof(Assists), new PropertyMetadata(false, OnUseDisabledDesignPropertyChanged));
+
+
+        private static void OnUseDisabledDesignPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FrameworkElement framework)
+            {
+                if ((bool)e.NewValue)
+                {
+                    framework.IsEnabledChanged += Framework_IsEnabledChanged;
+                    framework.Loaded += Framework_Loaded;
+                }
+                else
+                {
+                    framework.IsEnabledChanged -= Framework_IsEnabledChanged;
+                    framework.Loaded -= Framework_Loaded;
+                }
+            }
+            else
+            {
+                throw new Exception("Source Must be FrameworkElement");
+            }
+        }
+
+        private static void SetEnable(FrameworkElement framework) {
+            var adorner = (UnableAdorner)framework.GetOrAddAdorner(typeof(UnableAdorner));
+            adorner.SetEnable(framework.IsEnabled);
+        }
+
+        private static void Framework_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is FrameworkElement framework) {
+                SetEnable(framework);
+            }   
+        }
+
+        private static void Framework_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is FrameworkElement framework)
+            {
+                SetEnable(framework);
+            }
+        }
+        #endregion
+    }
+}
---
+++ DisabledCursor/UnableAdorner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DisabledCursor
+{
+    public sealed class UnableAdorner : Adorner
+    {
+        /// <summary>
+        /// 装饰器上的Visual集合
+        /// </summary>
+        private VisualCollection VisualCollection { get; set; }
+
+        protected override int VisualChildrenCount => VisualCollection.Count;
+
+        protected override Visual GetVisualChild(int index) => VisualCollection[index];
+
+
+        private readonly UnablePanel unablePanel;
+
+
+        public UnableAdorner(UIElement uIElement) : base(uIElement)
+        {
+            VisualCollection = new VisualCollection(this);
+            unablePanel = new UnablePanel();
+            VisualCollection.Add(unablePanel);
+        }
+
+        public void SetEnable(bool isEnable)
+        {
+            unablePanel.Visibility = isEnable ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            return base.MeasureOverride(constraint);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            unablePanel.Arrange(new Rect(finalSize));
+            return base.ArrangeOverride(finalSize);
+
+        }
+    }
+}
