3bc40ff727c63851c49aeb9cd6dc53915327aaea
390811879@qq.com <WangLan0913>
2024-01-15 17:55:47 +0800

    更新系统工程为最新模板类型

--- DisabledCursor/Properties/AssemblyInfo.cs
+++ DisabledCursor/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
@@ -8,11 +6,6 @@ using System.Windows.Markup;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("DisabledCursor")]
-[assembly: AssemblyDescription("提供禁用状态下的鼠标样式控制")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DisabledCursor")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,27 +25,14 @@ using System.Windows.Markup;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-[assembly:ThemeInfo(
+[assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //主题特定资源词典所处位置
-                             //(未在页面中找到资源时使用，
-                             //或应用程序资源字典中找到时使用)
+                                     //(未在页面中找到资源时使用，
+                                     //或应用程序资源字典中找到时使用)
     ResourceDictionaryLocation.SourceAssembly //常规资源词典所处位置
-                                      //(未在页面中找到资源时使用，
-                                      //、应用程序或任何主题专用资源字典中找到时使用)
+                                              //(未在页面中找到资源时使用，
+                                              //、应用程序或任何主题专用资源字典中找到时使用)
 )]
 
 [assembly: XmlnsDefinition("http://lan.design.com", "DisabledCursor")]
 [assembly: XmlnsPrefix("http://lan.design.com", "lan")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
-//通过使用 "*"，如下所示:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
